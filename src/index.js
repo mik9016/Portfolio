59bda543300afa82929a11c5024c45e6b565abce
mik9016 <mikolajgruszecki@o2.pl>
2021-02-19 11:43:42 +0100

    add new Project Description

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
 import style from "./scss/index.scss";
 
 
+const navigation = document.querySelector('#navigation');
+
+const tl = new TimeLineMax();
+
+tl.fromTo(navigation,1,{height: "0%"},{height:"80%"});
