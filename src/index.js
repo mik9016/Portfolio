e70fa809b21d53bf5a4f802b23bd9792b5e47527
mik9016 <mikolajgruszecki@o2.pl>
2021-02-20 22:50:02 +0100

    add form validation

--- src/index.js
+++ src/index.js
@@ -1,8 +1,54 @@
 import style from "./scss/index.scss";
+import anime from "animejs/lib/anime.es.js";
 
+//Form Handling
 
-const navigation = document.querySelector('#navigation');
+class Form {
+  constructor() {
+    this.nameInput = document.getElementById("name-input");
+    this.emailInput = document.getElementById("email-input");
+    this.messageInput = document.getElementById("message");
+    this.sendBtn = document.getElementById("send-btn");
 
-const tl = new TimeLineMax();
+    this.sendBtn.addEventListener("click", this.sendForm.bind(this));
+  }
 
-tl.fromTo(navigation,1,{height: "0%"},{height:"80%"});
+  validateForm() {
+    //  console.log(this.nameInput.length)
+    if (this.nameInput.value.length < 3) {
+      alert("To short name!");
+    } else if (this.messageInput.value.length < 6) {
+      alert("To short message!");
+    } else if (this.emailInput.value.length < 1) {
+      alert("Email field empty!");
+    }
+  }
+
+  clearForm() {
+    this.nameInput.value = "";
+    this.emailInput.value = "";
+    this.messageInput.value = "";
+  }
+
+  sendForm(event) {
+    event.preventDefault();
+    this.validateForm();
+    const filledForm = {
+      name: "",
+      email: "",
+      message: "",
+    };
+
+    filledForm.name = this.nameInput.value;
+    filledForm.email = this.emailInput.value;
+    filledForm.message = this.messageInput.value;
+
+    console.log(filledForm);
+
+    //fetch
+
+    this.clearForm();
+  }
+}
+//
+new Form();
